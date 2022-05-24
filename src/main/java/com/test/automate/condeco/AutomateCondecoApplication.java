9647c496a1d3a671873cac73ab0e58a56550f6ce
Arpit Aggarwal <arpitaggarwal@Arpits-MacBook-Pro.local>
2022-05-24 20:35:38 +0400

    first commit

---
+++ src/main/java/com/test/automate/condeco/AutomateCondecoApplication.java
@@ -0,0 +1,13 @@
+package com.test.automate.condeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomateCondecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomateCondecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/automate/condeco/AutomateCondecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.automate.condeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomateCondecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
